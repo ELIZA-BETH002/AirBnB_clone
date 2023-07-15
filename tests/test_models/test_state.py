8354e6702e73a7b56713660513f3afa8feaaf883
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-07-15 18:30:13 +0100

    test state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Defines unittests for models/state.py.
+
+"""
+Defines unittests for models/state.py.
 
 Unittest classes:
     TestState_instantiation
     TestState_save
     TestState_to_dict
 """
+
+
 import os
 import models
 import unittest
